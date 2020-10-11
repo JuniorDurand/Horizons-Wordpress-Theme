f5455f27e8ae8e626d00b546b2c1c1fdfdc249a2
JuniorDurand <junior_durand@outlook.com>
2020-10-11 19:51:55 -0300

    fix(page-and-single): fix indentation of code.

--- single.php
+++ single.php
@@ -1,46 +1,37 @@
 <?php
-
 get_header();
 echo "<!-- single.php -->";
 ?>
 
 
 <div id="main" class="wrapper style1">
-				<div class="container">
-                <section>
-						<header class="major">
-                            <h2><?php wp_title(''); ?></h2>
-						</header>
-                        
-                        <?php
-                            if (have_posts()) :
-                                while (have_posts()) : the_post(); ?>
+    <div class="container">
+        <section>
+            <header class="major">
+                <h2><?php the_title(); ?></h2>
+            </header>
+                
+            <?php
+            if (have_posts()) :
+                while (have_posts()) : the_post(); ?>
+            
+                <?php 
+                    $content = get_the_content();
+                    echo $content;
+                ?>
 
-                                    
-                                    <?php 
-                                        $content = get_the_content();
-                                        echo $content;
+                <?php endwhile;
 
-                                    ?>
+            else :
+                echo '<p> No content found</p>';
 
-                                <?php endwhile;
+            endif;
+            ?>
 
-                            else :
-                                echo '<p> No content found</p>';
-
-                            endif;
-                        ?>
-
-                    </section>
-				</div>
-            </div>
-
-
-
-
+        </section>
+    </div>
+ </div>
 
 <?php
-
 get_footer();
-
 ?>
